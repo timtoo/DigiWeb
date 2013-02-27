f558fa3ca1487567b819ca0bdf78a655246957d9
Tim Middleton <tim@vaults.ca>
2013-02-26 23:20:44 -0500

    .

--- dev/make.sh
+++ dev/make.sh
@@ -10,23 +10,36 @@
 #
 # Note: Assumes pip and virtualenv are already installed
 
+BASEDIR=$(dirname $0)
+
+PIP=`which pip`
+VIRTUALENV=`which virtualenv`
+APTGET=`which apg-get`
+
 # ensure path argument is provided
 if [ "$1" != "" ]; then
 
     # install apt-get packages if any
     if [ -f dpkg.txt ]; then
-        cat dpkg.txt | (while read a; do sudo apt-get -y install $a; done)
+        cat dpkg.txt | (while read a; do sudo $APTGET -y install $a; done)
     fi
 
     # create and activate virtualenv
-    virtualenv "$1"
+    $VIRTUALENV "$1"
     . "$1/bin/activate"
 
-    # install python packages
-    if [ -f pypi.txt ]; then
-        pip install --upgrade -r pypi.txt
+    if [ "$VIRTUAL_ENV" != "" ]; then
+
+        # install python packages
+        if [ -f pypi.txt ]; then
+            $PIP install --upgrade -r pypi.txt
+        fi
+
+    else
+        echo "VIRTUAL_ENV not active, skipping installing pypi packages."
     fi
 
 else
     echo "Provide path for build environment"
 fi
+
